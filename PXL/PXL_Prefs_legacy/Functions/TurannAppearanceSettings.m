193c0cd4da435b1fa43bdfb934c0fb8b55bcc663
Turann_ <84424530+turannul@users.noreply.github.com>
2024-03-01 03:40:08 +0300

    Prefs are still (fucked) sad. 😢

---
+++ PXL/PXL_Prefs_legacy/Functions/TurannAppearanceSettings.m
@@ -0,0 +1,36 @@
+#import "Turann.h"
+
+@implementation TurannAppearanceSettings
+/*
+-(UIColor *)tintColor { return [UIColor colorWithRed: 0.20 green: 1.0 blue: 0.0 alpha: 0.88]; }
+-(UIColor *)statusBarTintColor { return [UIColor blackColor]; }
+-(UIColor *)navigationBarTitleColor { return [UIColor blackColor]; }
+-(UIColor *)navigationBarBackgroundColor { return [UIColor blueColor];//colorWithRed: 0.00 green: 0.20 blue: 0.31 alpha: 1.00]; }
+-(BOOL)translucentNavigationBar { return YES; }
+-(UIUserInterfaceStyle)userInterfaceStyle { return 2; }
+-(UIColor *)tableViewCellSeparatorColor { return [UIColor blackColor]; }
+-(UIColor *)tableViewBackgroundColor { return [UIColor blackColor]; }
+-(UIColor *)tableViewCellSelectionColor { return [UIColor greenColor]; }
+-(UIColor *)tableViewCellBackgroundColor { return [UIColor blackColor]; }
+-(UIColor *)tableViewCellTextColor{ return [UIColor greenColor]; }
+	appearanceSettings = [[HBAppearanceSettings alloc] init];
+		appearanceSettings.tintColor = [UIColor colorWithRed:66.f / 255.f green:105.f / 255.f blue:154.f / 255.f alpha:1];
+		appearanceSettings.barTintColor = [UIColor systemRedColor];
+		appearanceSettings.navigationBarTitleColor = [UIColor whiteColor];
+		appearanceSettings.tableViewBackgroundColor = [UIColor colorWithWhite:242.f / 255.f alpha:1];
+		appearanceSettings.statusBarStyle = UIStatusBarStyleLightContent;
+		self.hb_appearanceSettings = appearanceSettings;
+	appearanceSettings.tintColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.statusBarStyle = UIStatusBarStyleLightContent;
+	appearanceSettings.navigationBarTitleColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.navigationBarBackgroundColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.tableViewBackgroundColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.tableViewCellSelectionColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.tableViewCellBackgroundColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.tableViewCellTextColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	appearanceSettings.tableViewCellSeparatorColor = [UIColor colorWithRed: 0.20 green: 0.35 blue: 0.80 alpha: 0.88];
+	if (@available(iOS 13, *))
+		appearanceSettings.userInterfaceStyle = 2;
+	appearanceSettings.translucentNavigationBar = YES;
+	*/
+@end
---
+++ PXL/PXL_Prefs_legacy/PXLPrefs.h
@@ -0,0 +1,8 @@
+#import <Preferences/PSListController.h>
+#import <Foundation/Foundation.h>
+#import "Functions/Turann.h"
+#import "Functions/SparkColourPickerUtils.h"
+@interface PXLPrefs:Turann{}
+@property (nonatomic, retain) UIBarButtonItem *respringApplyButton;
+@property (nonatomic, retain) UIBarButtonItem *respringConfirmButton;
+@end
